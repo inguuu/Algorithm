d87a3600f88e4f968157aa66ea255cef23e7547a
inguuu <dlsrb8949@gmail.com>
2019-09-23 21:13:32 +0900

    0923

--- algorithm/stack&queue/괄호검사.cpp
+++ algorithm/stack&queue/괄호검사.cpp
@@ -1,61 +1,32 @@
+
 #include <stdio.h>
-#include <stack>
 #include <string.h>
-#include <algorithm>
 
-using namespace std;
-
-char order[28];
-char alpha[28];
 int main() {
- int oidx=0;
- int aidx=0;
-  stack<char> s;
 
-  scanf("%s",order);
-  int len = strlen(order);
+  int idx=0;
+  
+  char str[52];
+  scanf("%s",str);
+  
+  int len = strlen(str);
   
   for(int i=0;i<len;i++){
-    alpha[i]=order[i];
-  }
-  
-  sort(alpha,alpha+len);
-  int result[100]={0,};
-  int ridx =0;
-  while(1){
-    if(s.empty()){
-        s.push(alpha[aidx]);
-        result[ridx]=1;
-        ++aidx;
-        ++ridx;
-    }
-    if(s.top()==order[oidx]){
-     result[ridx]=2;
-      ++ridx;
-      ++oidx;
-      s.pop();
+    if(str[i]=='('){
+      ++idx;
     }else{
-      if(aidx+1>len){
-        printf("impossible");
-        return 0;
-      }else{
-        s.push(alpha[aidx]);
-        
-        result[ridx]=1;
-        ++ridx;
-        ++aidx;
-      }
-      
+      --idx;
     }
-    if(oidx==len)break;
-  
-  }
-  for(int i=0;i<100;i++){
-    if(result[i]==1){
-      printf("push\n");
-    }else if(result[i]==2){
-      printf("pop\n");
+    if(idx<0){
+      printf("NO");
+      return 0;
     }
   }
+  if(idx==0){
+     printf("YES");
+  }else{
+     printf("NO");
+  }
+
   return 0;
 }
